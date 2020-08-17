d02560e59a16f323c13ffa42de1524363a50bb59
Romit Choudhary <choudharyromit@gmail.com>
2020-08-17 21:19:10 +0530

    Updates

---
+++ precache-manifest.13d4e0383f48121aefc6f7181a9dc0f1.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1023ccd29bf691cf6ec257830de0d3da",
+    "url": "/manual-landing/index.html"
+  },
+  {
+    "revision": "b89362ef27ba0c6090d8",
+    "url": "/manual-landing/static/css/main.64ad55af.chunk.css"
+  },
+  {
+    "revision": "42a97371284e7a8c923a",
+    "url": "/manual-landing/static/js/2.27bcbf55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/manual-landing/static/js/2.27bcbf55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89362ef27ba0c6090d8",
+    "url": "/manual-landing/static/js/main.8e72d184.chunk.js"
+  },
+  {
+    "revision": "85f79d374f9a76ccbfe4",
+    "url": "/manual-landing/static/js/runtime-main.f8b1616c.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/manual-landing/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/manual-landing/static/media/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/manual-landing/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/manual-landing/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/manual-landing/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/manual-landing/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/manual-landing/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "83d4968b8ed17978d4b55cad6c4d1bc0",
+    "url": "/manual-landing/static/media/facebook.83d4968b.svg"
+  },
+  {
+    "revision": "7d4a1a79d58aba86ee20112964d6863d",
+    "url": "/manual-landing/static/media/hair-cat-header.7d4a1a79.jpg"
+  },
+  {
+    "revision": "49f0f69648feba30ea727f72e2af3d2d",
+    "url": "/manual-landing/static/media/hair-cat-header@2x.49f0f696.jpg"
+  },
+  {
+    "revision": "b0926c990f6c948eced1b8a7234230bc",
+    "url": "/manual-landing/static/media/hair-photo.b0926c99.jpg"
+  },
+  {
+    "revision": "a75c9c40a94f7721529034a075923b84",
+    "url": "/manual-landing/static/media/hair-photo@2x.a75c9c40.jpg"
+  },
+  {
+    "revision": "02f072d4b8097cdf0c9851573691b6a1",
+    "url": "/manual-landing/static/media/hair-photo@3x.02f072d4.jpg"
+  },
+  {
+    "revision": "aa47966c97f8528af24bee2d049a14d1",
+    "url": "/manual-landing/static/media/linkedin.aa47966c.svg"
+  },
+  {
+    "revision": "efdf6db44ba34dfa1ebe6037439c8f81",
+    "url": "/manual-landing/static/media/manual-symbol-large.efdf6db4.svg"
+  },
+  {
+    "revision": "7c9dbfabc2be99337c02afecdeaed171",
+    "url": "/manual-landing/static/media/manual-symbol.7c9dbfab.svg"
+  },
+  {
+    "revision": "41a7d76b6ad1e613e9e6b0c1f3e94e4d",
+    "url": "/manual-landing/static/media/sex-photo.41a7d76b.jpg"
+  },
+  {
+    "revision": "af24d859a3ce58e26a1b085c73792e79",
+    "url": "/manual-landing/static/media/sex-photo@2x.af24d859.jpg"
+  },
+  {
+    "revision": "d8f6ff934b669ccd6ba66a7bca7de6a5",
+    "url": "/manual-landing/static/media/sex-photo@3x.d8f6ff93.jpg"
+  },
+  {
+    "revision": "b16780f681809c61e0e106d063011e44",
+    "url": "/manual-landing/static/media/twitter.b16780f6.svg"
+  }
+]);
